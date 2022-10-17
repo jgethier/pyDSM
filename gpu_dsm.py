d7e30cef328a869dcf8edb7a57f8aa60cacfe2d9
unknown <jgethier@gmail.com>
2022-10-17 16:09:12 -0400

    updated flag arguments

--- gpu_dsm.py
+++ gpu_dsm.py
@@ -5,19 +5,19 @@ def gpu_dsm():
 
 	parser = argparse.ArgumentParser(description='Run pyDSM with specified arguments.')
 
-	parser.add_argument('sim_ID', type=int, nargs='?',default=0,
+	parser.add_argument('ID', type=int, nargs='?',default=0,
                     help='An integer for the simulation ID.')
-	parser.add_argument('-d', type=int, nargs='?',default=0,
+	parser.add_argument('-d', metavar='device_num', type=int, nargs='?',default=0,
                     help='An integer for the device ID.')
-	parser.add_argument('-c', type=str, nargs='?',default='MuNCH',
-                    help='Which correlator to use (otf or munch).')
-	parser.add_argument('-o', type=str, nargs='?',default='./DSM_results',
+	parser.add_argument('-c', type=str, nargs='?',default='MuNCH',choices=['otf','munch'],
+                    help='Specify which correlator to use (otf or munch).')
+	parser.add_argument('-o', metavar='path/to/output/',type=str, nargs='?',default='./DSM_results',
 					help='Specify output directory.')
-	parser.add_argument("--fit", action="store_true", help='A flag to turn on G(t) fit.')
+	parser.add_argument("-f", "--fit", action="store_true", help='A flag to turn on G(t) fit.')
 
 	args = parser.parse_args()
 
-	sim_ID = args.sim_ID
+	sim_ID = args.ID
 	device_ID = args.d
 	output_dir = args.o
 	correlator = args.c 

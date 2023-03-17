8398061f1b9a43695d8ed6636176c68ea3262875
unknown <jgethier@gmail.com>
2023-03-17 16:38:50 -0400

    updated MUnCH correlator

--- gpu_dsm.py
+++ gpu_dsm.py
@@ -13,6 +13,8 @@ def gpu_dsm():
                     help='Specify which correlator to use (otf or munch).')
 	parser.add_argument('-o', metavar='path/to/output/',type=str, nargs='?',default='./DSM_results',
 					help='Specify output directory.')
+	parser.add_argument("-r", "--raw", action="store_true", 
+					help='A flag to save raw results to file (storage files may become large).')
 	parser.add_argument("-f", "--fit", action="store_true", 
 					help='A flag to turn on G(t) fit.')
 	parser.add_argument('--distr',action="store_true",
@@ -27,13 +29,14 @@ def gpu_dsm():
 	sim_ID = args.ID
 	device_ID = args.d
 	output_dir = args.o
-	correlator = args.c 
+	correlator = args.c
+	save_rawdata = args.raw  
 	fit = args.fit 
 	distr= args.distr
 	load_file = args.load
 	save_file = args.save
 
-	run_dsm = FSM_LINEAR(sim_ID,device_ID,output_dir,correlator,fit,distr,load_file,save_file)
+	run_dsm = FSM_LINEAR(sim_ID,device_ID,output_dir,correlator,save_rawdata,fit,distr,load_file,save_file)
 	run_dsm.run()
 
 	return
